20fe608bcc6b3fbc5ab6cdea7a36abf550d88058
hx2003 <hx2003@Ubuntu.myguest.virtualbox.org>
2023-08-14 22:16:35 +0800

    Update

--- src/tt_um_stepper_driver.v
+++ src/tt_um_stepper_driver.v
@@ -51,6 +51,8 @@ module tt_um_stepper_driver #( parameter MAX_COUNT = 24'd10_000_000 ) (
         if (reset) begin
             step_pos <= 0;
             start <= 0;
+            test_number_a <= 0;
+            test_number_b <= 0;
             
         end else begin
             if (io_step_rising)
